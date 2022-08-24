fcb2980fe1075e00a031109d9539e7f3d626a3e9
Carlos <carloshlzansavio@gmail.com>
2022-08-24 16:00:25 +0100

    add /auth/login

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AuthAPIView
+from .views import AuthAPIView, LoginAPIView
 
 app_name = 'authentication'
 urlpatterns = [
     path('/', AuthAPIView.as_view()),
+    path('/login', LoginAPIView.as_view()),
 ]
--- api/authentication/views.py
+++ api/authentication/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import (LoginSerializer)
+
 # Create your views here.
 
 
@@ -12,3 +14,17 @@ class AuthAPIView(APIView):
 
     def get(self, request):
         return Response({'ok'}, status=status.HTTP_200_OK)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+        print(user)
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
