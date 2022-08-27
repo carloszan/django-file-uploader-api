294a3efe3d05e90276bd12b03a0004e149f7035b
Carlos <carloshlzansavio@gmail.com>
2022-08-27 16:56:13 +0100

    add attribute type to file

--- api/files/models.py
+++ api/files/models.py
@@ -4,6 +4,7 @@ from api.authentication.models import User
 
 class File(models.Model):
     name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
     file_url = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
